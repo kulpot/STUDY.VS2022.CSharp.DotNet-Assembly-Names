6d3fb5c9ff51a719b112a92581d7cddc6030eee4
Kulpot <kulpotwahu@gmail.com>
2023-09-25 05:48:56 +0800

    Add project files.

---
+++ DotNet Assembly Names/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=pschC5-4ao0&list=PLRwVmtr-pp06rfSgNYu_oBg40DkwXiRHt&index=13
+//
+
+/*----------------------------CMD----------------------------
+ * 
+C:\Users\sunny\source\repos\DotNet Assembly Names>dir
+ Volume in drive C has no label.
+ Volume Serial Number is DAE4-938D
+
+ Directory of C:\Users\sunny\source\repos\DotNet Assembly Names
+
+25/09/2023  05:46 am    <DIR>          .
+25/09/2023  05:46 am    <DIR>          ..
+25/09/2023  05:47 am    <DIR>          DotNet Assembly Names
+25/09/2023  05:46 am             1,169 DotNet Assembly Names.sln
+               1 File(s)          1,169 bytes
+               3 Dir(s)  489,949,769,728 bytes free
+
+C:\Users\sunny\source\repos\DotNet Assembly Names>cd DotNet Assembly Names
+
+C:\Users\sunny\source\repos\DotNet Assembly Names\DotNet Assembly Names>dir
+ Volume in drive C has no label.
+ Volume Serial Number is DAE4-938D
+
+ Directory of C:\Users\sunny\source\repos\DotNet Assembly Names\DotNet Assembly Names
+
+25/09/2023  05:47 am    <DIR>          .
+25/09/2023  05:47 am    <DIR>          ..
+25/09/2023  05:45 am               189 App.config
+25/09/2023  05:45 am    <DIR>          bin
+25/09/2023  05:46 am             2,392 DotNet Assembly Names.csproj
+25/09/2023  05:45 am    <DIR>          obj
+25/09/2023  05:47 am               192 Program.cs
+25/09/2023  05:45 am    <DIR>          Properties
+               3 File(s)          2,773 bytes
+               5 Dir(s)  489,948,942,336 bytes free
+
+
+
+ * 
+ */
+
+
+class MainClass
+{
+    static void Main()
+    {
+
+    }
+}
